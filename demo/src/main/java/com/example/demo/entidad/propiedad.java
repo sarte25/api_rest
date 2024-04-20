0a3bb85649dc1a4d9a85b22ad692b8c34fa8ed55
sarte25 <sebasmena.121@gmail.com>
2024-04-20 11:31:57 -0600

    primera subida de los archivos en los cuales se creo la entidad y se dejo inicializado la interface de repositorio

---
+++ demo/src/main/java/com/example/demo/entidad/propiedad.java
@@ -0,0 +1,106 @@
+package com.example.demo.entidad;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class propiedad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(unique = true )
+    @NotBlank(message = "El nombre no puede estar vacio")
+    private String nombre;
+
+    @NotBlank(message = "La ubicacion no puede estar vacio")
+    private String ubicacion;
+
+    @NotBlank(message = "La URL de la imagen no puede estar vacia")
+    private String imageurl;
+
+
+    private boolean disponibilidad;
+
+    @NotNull(message = "El precio no puede ser nulo")
+    @Positive(message = "El precio debe ser mayor a cero")
+    private double precio;
+
+    @NotNull(message = "La fecha de creacion no puede ser nulo")
+    private LocalDateTime createdAt;
+
+    public propiedad() {
+
+    }
+
+    public propiedad(long id, String nombre, String ubicacion, String imageurl, boolean disponibilidad, double precio, LocalDateTime createdAt) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.imageurl = imageurl;
+        this.disponibilidad = disponibilidad;
+        this.precio = precio;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
